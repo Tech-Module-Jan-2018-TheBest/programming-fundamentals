3cdf712105ebc45ea1edbeb7f86d7ea30b0ccd99
viktoriyadi <vikstoyyy@abv.bg>
2018-03-10 18:03:18 +0200

    1. I created a new folder called intro-and-basic-syntax. 2. In this folder I created a folder called “exercise”. 3. In this folder I have created 5 folders. 4. In first folder I put my solution for Problem 1: Debit card numbers. Your first task is: Everyone has to choose a folder and put solution for the problem.

---
+++ intro-and-basic-syntax/exercise/01. Debit card number/DebitCardNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Problem_1.Debit_Card_Number
+{
+    class DebitCardNumber
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            Console.Write($"{num1:d4} {num2:d4} {num3:d4} {num4:d4}");
+        }
+    }
+}
