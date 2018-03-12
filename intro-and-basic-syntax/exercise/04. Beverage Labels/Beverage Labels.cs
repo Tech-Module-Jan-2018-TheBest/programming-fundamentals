19aacc1ee416fab68c65bac632585c733e5226c6
viktoriyadi <vikstoyyy@abv.bg>
2018-03-12 22:02:01 +0200

    Change a variable name in the problem 4

--- intro-and-basic-syntax/exercise/04. Beverage Labels/Beverage Labels.cs
+++ intro-and-basic-syntax/exercise/04. Beverage Labels/Beverage Labels.cs
@@ -11,21 +11,21 @@ namespace _04.Beverage_Labels
         static void Main(string[] args)
         {
             //We recieve from the Console the product name -> a string;
-            string bevarage = Console.ReadLine();
+            string productName = Console.ReadLine();
 
             /* In the next three lines, we recieve volume,
              * energy and sugar content of our product,
              * as and double */
-            double volume = double.Parse(Console.ReadLine());
-            double energy = double.Parse(Console.ReadLine());
-            double sugar = double.Parse(Console.ReadLine());
+            double productVolume = double.Parse(Console.ReadLine());
+            double productEnergy = double.Parse(Console.ReadLine());
+            double productSugar = double.Parse(Console.ReadLine());
 
             //We calculate energy and sugar;
-            double energyTotal = energy * volume / 100;
-            double sugarTotal = sugar * volume / 100;
+            double energy = productEnergy * productVolume / 100;
+            double sugars = productSugar * productVolume / 100;
 
             //We print the result;
-            Console.WriteLine($"{volume}ml {bevarage}:\r\n{energyTotal}kcal, {sugarTotal}g sugars");
+            Console.WriteLine($"{productVolume}ml {productName}:\r\n{energy}kcal, {sugars}g sugars");
 
         }
     }
