174cab563fce3bcda8d4a2c515229db66b6669be
viktoriyadi <vikstoyyy@abv.bg>
2018-03-12 21:42:29 +0200

    Document the code of the problem 2;

--- intro-and-basic-syntax/exercise/02. Rectangle Area/Rectangle Area.cs
+++ intro-and-basic-syntax/exercise/02. Rectangle Area/Rectangle Area.cs
@@ -6,9 +6,11 @@ namespace _02._Rectangle_Area
     {
         static void Main(string[] args)
         {
+            //In the next two lines, we recieve from the Console width and height of a rectangle;
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
 
+            //We calculate the area of the rectangle;
             Console.WriteLine($"{a*b:f2}");
         }
     }
