a4b847018034ccc27cdabbaba29ab3a61a1378c6
viktoriyadi <vikstoyyy@abv.bg>
2018-03-12 21:16:51 +0200

    Added problem 3.

---
+++ intro-and-basic-syntax/exercise/03 Miles to Kilometers/Miles-to-Kilometres.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problem_3.Miles_to_Kilometers
+{
+    class MilesToKilometres
+    {
+        static void Main(string[] args)
+        {
+            double mile = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{(mile * 1.60934):f2}");
+        }
+    }
+}
