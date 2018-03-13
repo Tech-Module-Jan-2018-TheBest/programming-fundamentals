e961dbc028b9907b56cc58d234888154cbc1dff5
Teodor Chakarov <halflife987@abv.bg>
2018-03-13 13:50:29 +0200

    added new variable names in problem 3

--- intro-and-basic-syntax/exercise/03 Miles to Kilometers/Miles-to-Kilometres.cs
+++ intro-and-basic-syntax/exercise/03 Miles to Kilometers/Miles-to-Kilometres.cs
@@ -7,7 +7,7 @@ namespace Problem_3.Miles_to_Kilometers
         static void Main(string[] args)
         {
             //Here we take number(miles) from the console and parse it to double number.
-            double mile = double.Parse(Console.ReadLine());
+            double distanceInMiles = double.Parse(Console.ReadLine());
 
             //Then the number(miles) is multiplied by 1.60934,and printed to the 2nd decimal place on the console. 
             Console.WriteLine($"{(mile * 1.60934):f2}");
