a0ebbf29571d0a8f522b7ba73b9dd8b58f0bde52
data47 <sin47@abv.bg>
2018-03-13 14:01:19 +0200

    Documented Problems 5's code.

--- intro-and-basic-syntax/exercise/05. Character Stats/DrawingStats.cs
+++ intro-and-basic-syntax/exercise/05. Character Stats/DrawingStats.cs
@@ -6,18 +6,23 @@ namespace DrawingStats
     {
         static void Main(string[] args)
         {
+            /*First we recieve from the console 5 entries - name, 
+             * current health, max health, current energy and max energy */
             String championName = Console.ReadLine();
             int totalHealth = int.Parse(Console.ReadLine());
             int fullHealth = int.Parse(Console.ReadLine());
             int totalEnergy = int.Parse(Console.ReadLine());
             int fullEnergy = int.Parse(Console.ReadLine());
 
+            // We create two new strings representing the health bars;
             string remainingHealth = new string('|', totalHealth);
             string missingHealth = new string('.', fullHealth - totalHealth);
 
+            // We create two new strings representing the energy bars;
             string remainingEnergy = new string('|', totalEnergy);
             string missingEnergy = new string('.', fullEnergy-totalEnergy);
 
+            //On the console we print the wanted output;
             Console.WriteLine("Name: {0}",championName);
             Console.WriteLine("Health: |{0}{1}|",remainingHealth,missingHealth);
             Console.WriteLine("Energy: |{0}{1}|",leftedEnergy,missingEnergy);
